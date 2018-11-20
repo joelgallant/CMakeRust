5fcd098113c6150cd085b3d69a92a1e7ab237fbf
Joel Gallant <joel@joelgallant.me>
2018-11-20 10:13:58 -0700

    Adds PACKAGE and PWD settings to allow specific rust packages and non-cwd builds

--- cmake/CMakeCargo.cmake
+++ cmake/CMakeCargo.cmake
@@ -1,5 +1,5 @@
 function(cargo_build)
-    cmake_parse_arguments(CARGO "" "NAME" "" ${ARGN})
+  cmake_parse_arguments(CARGO "" "NAME;PACKAGE;PWD" "" ${ARGN})
     string(REPLACE "-" "_" LIB_NAME ${CARGO_NAME})
 
     set(CARGO_TARGET_DIR ${CMAKE_CURRENT_BINARY_DIR})
@@ -24,7 +24,7 @@ function(cargo_build)
 		set(LIB_TARGET "universal")
     elseif(CMAKE_SYSTEM_NAME STREQUAL Darwin)
         set(LIB_TARGET "x86_64-apple-darwin")
-	else()
+    else()
         if(CMAKE_SIZEOF_VOID_P EQUAL 8)
             set(LIB_TARGET "x86_64-unknown-linux-gnu")
         else()
@@ -42,12 +42,12 @@ function(cargo_build)
 
     set(LIB_FILE "${CARGO_TARGET_DIR}/${LIB_TARGET}/${LIB_BUILD_TYPE}/${CMAKE_STATIC_LIBRARY_PREFIX}${LIB_NAME}${CMAKE_STATIC_LIBRARY_SUFFIX}")
 
-	if(IOS)
-		set(CARGO_ARGS "lipo")
-	else()
-    	set(CARGO_ARGS "build")
-		list(APPEND CARGO_ARGS "--target" ${LIB_TARGET})
-	endif()
+    if(IOS)
+        set(CARGO_ARGS "lipo")
+    else()
+        set(CARGO_ARGS "build")
+        list(APPEND CARGO_ARGS "--target" ${LIB_TARGET})
+    endif()
 
     if(${LIB_BUILD_TYPE} STREQUAL "release")
         list(APPEND CARGO_ARGS "--release")
@@ -57,14 +57,25 @@ function(cargo_build)
 
     set(CARGO_ENV_COMMAND ${CMAKE_COMMAND} -E env "CARGO_TARGET_DIR=${CARGO_TARGET_DIR}")
 
+    if(CARGO_PACKAGE)
+        list(APPEND CARGO_ARGS "-p ${CARGO_PACKAGE}")
+    endif()
+
+    if(CARGO_PWD)
+      file(TO_CMAKE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/${CARGO_PWD} CARGO_PWD)
+    else()
+      set(CARGO_PWD ${CMAKE_CURRENT_SOURCE_DIR})
+    endif()
+
     add_custom_command(
         OUTPUT ${LIB_FILE}
         COMMAND ${CARGO_ENV_COMMAND} ${CARGO_EXECUTABLE} ARGS ${CARGO_ARGS}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        WORKING_DIRECTORY ${CARGO_PWD}
         DEPENDS ${LIB_SOURCES}
         COMMENT "running cargo")
-    add_custom_target(${CARGO_NAME}_target ALL DEPENDS ${LIB_FILE})
+
     add_library(${CARGO_NAME} STATIC IMPORTED GLOBAL)
-    add_dependencies(${CARGO_NAME} ${CARGO_NAME}_target)
     set_target_properties(${CARGO_NAME} PROPERTIES IMPORTED_LOCATION ${LIB_FILE})
+    add_custom_target(${CARGO_NAME}-target ALL DEPENDS ${LIB_FILE})
+    add_dependencies(${CARGO_NAME} ${CARGO_NAME}-target)
 endfunction()
